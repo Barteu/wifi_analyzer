0238efb5c924365b590ca4163d2cc905e829175b
Bartłomiej Szymkowiak <61190121+Barteu@users.noreply.github.com>
2022-05-24 13:01:18 +0000

    Update print_funs.h

--- print_funs.h
+++ print_funs.h
@@ -7,4 +7,5 @@
 void print_frame_body(const u_char *frame_body, int len);
 void print_type(u_int8_t type);
 void print_subtype(u_int8_t type, u_int8_t subtype);
+void print_mac_addr(u_int8_t addr[]);
 void print_wifi_hdr(struct wifi_hdr wifihdr );
